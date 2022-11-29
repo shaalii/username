c927f1a82e45da18a85081c5814783fca08bf37b
Shahzaib Ali <shaali13@publicisgroupe.net>
2022-11-30 00:10:31 +0530

    first commit

---
+++ src/test/java/userTest/Userdetail.java
@@ -0,0 +1,15 @@
+package userTest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Userdetail {
+	
+	@Test
+	
+	public void username() {
+		System.out.println("Shahzaib Ali");
+		Assert.assertTrue(true);
+	}
+
+}
